9f458c71a4c1fe9eb9888ea116ff64c752d6c4f7
ceco851ba <ceco851ba@gmail.com>
2022-12-03 10:46:48 +0100

    hw monitor test

---
+++ lcd.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+from RPLCD.i2c import CharLCD
+from gpiozero import CPUTemperature
+import psutil
+from psutil._common import bytes2human
+import time
+import datetime
+now = datetime.datetime.now()
+lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
+              cols=16, rows=2, dotsize=8,
+              charmap='A02',
+              auto_linebreaks=True,
+              backlight_enabled=False)
+lcd.clear()
+
+
+
+while True:
+    cpu = str(psutil.cpu_percent()) + '%'
+    temp = str(CPUTemperature().temperature) + '°C' 
+    ramUsed = (psutil.virtual_memory()[3])
+    ramFree = (psutil.virtual_memory()[4])
+
+    #ramUsed = 'RAM Used ' + str(bytes2human(ramUsed))
+    #ramFree = 'RAM Free ' + str(bytes2human(ramFree))
+    ramUsed = (bytes2human(ramUsed))
+    ramFree = (bytes2human(ramFree))
+    #rampct = 'RAM Used (GB):' + str(psutil.virtual_memory()[3]/1000000000)
+    # cpu used
+    lcd.clear()
+    lcd.cursor_pos = (0, 0)
+    lcd.write_string('CPU Usage: ')
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string(cpu)
+    time.sleep(3)
+    # cpu temp
+    lcd.clear()
+    lcd.cursor_pos = (0, 0)
+    lcd.write_string('CPU Temperature: ')
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string(temp)
+    time.sleep(3)
+    # ram free 
+    lcd.clear()
+    lcd.cursor_pos = (0, 0)
+    lcd.write_string('Memory Free')
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string(ramFree)
+    time.sleep(3)
+    # ram used
+    lcd.clear()
+    lcd.cursor_pos = (0, 0)
+    lcd.write_string('Memory Used')
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string(ramUsed)
+    time.sleep(3)
+
+
+    
+
+
+
+
+
+
+
+
