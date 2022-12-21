6d5f42e695f6033f89da42df2fa8a7e0d5f453f7
ceco851ba <ceco851ba@gmail.com>
2022-12-21 12:29:22 +0100

    add current time

--- lcd.py
+++ lcd.py
@@ -5,8 +5,7 @@ from gpiozero import CPUTemperature
 import psutil
 from psutil._common import bytes2human
 import time
-import datetime
-now = datetime.datetime.now()
+from datetime import datetime
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
               cols=16, rows=2, dotsize=8,
               charmap='A00',
@@ -17,16 +16,28 @@ lcd.clear()
 
 
 while True:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
     cpu = str(psutil.cpu_percent()) + '%'
     temp = str(int(CPUTemperature().temperature))+ '°C'
     ramUsed = (psutil.virtual_memory()[3])
     ramFree = (psutil.virtual_memory()[4])
+
 
     #ramUsed = 'RAM Used ' + str(bytes2human(ramUsed))
     #ramFree = 'RAM Free ' + str(bytes2human(ramFree))
     ramUsed = (bytes2human(ramUsed))
     ramFree = (bytes2human(ramFree))
     #rampct = 'RAM Used (GB):' + str(psutil.virtual_memory()[3]/1000000000)
+    
+    # time
+    lcd.clear()
+    lcd.cursor_pos = (0, 0)
+    lcd.write_string('Time: ')
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string(current_time)
+    time.sleep(3)
+    
     # cpu used
     lcd.clear()
     lcd.cursor_pos = (0, 0)
