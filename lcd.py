cf02bb6a218ef35063d69b795a1a763d3b933c24
ceco851ba <ceco851ba@gmail.com>
2023-01-04 22:09:00 +0100

    add some features

--- lcd.py
+++ lcd.py
@@ -5,6 +5,7 @@ from gpiozero import CPUTemperature
 import psutil
 from psutil._common import bytes2human
 import time
+import socket
 from datetime import datetime
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
               cols=16, rows=2, dotsize=8,
@@ -13,59 +14,78 @@ lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
               backlight_enabled=True)
 lcd.clear()
 
+Transition = 10
+Refresh = .5
 
 
 while True:
-    now = datetime.now()
-    current_time = now.strftime("%H:%M")
-    cpu = str(psutil.cpu_percent()) + '%'
-    temp = str(int(CPUTemperature().temperature))+ '°C'
-    ramUsed = (psutil.virtual_memory()[3])
-    ramFree = (psutil.virtual_memory()[4])
-
-
     #ramUsed = 'RAM Used ' + str(bytes2human(ramUsed))
     #ramFree = 'RAM Free ' + str(bytes2human(ramFree))
-    ramUsed = (bytes2human(ramUsed))
-    ramFree = (bytes2human(ramFree))
+    
     #rampct = 'RAM Used (GB):' + str(psutil.virtual_memory()[3]/1000000000)
     
     # time
-    lcd.clear()
-    lcd.cursor_pos = (0, 0)
-    lcd.write_string('Time: ')
-    lcd.cursor_pos = (1, 0)
-    lcd.write_string(current_time)
-    time.sleep(3)
+    for _ in range(Transition):
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        #lcd.clear()
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string('Time: ')
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string(current_time)
+        time.sleep(Refresh)
+    else:
+        lcd.clear()
+    # cpu usage and temperature 
+    for _ in range(Transition):
+        cpuusg = str(psutil.cpu_percent()) + '%'
+        cputemp = str(int(CPUTemperature().temperature))+ '°C'
+
+        #lcd.clear()
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string('CPU Usage&Temp')
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string('U:'+cpuusg+' T:'+cputemp)
+        time.sleep(Refresh)
+    else:
+        lcd.clear()
+    # ram
+    for _ in range(Transition):
+        ramFree = (bytes2human(psutil.virtual_memory()[4]))
+        ramUsed = (bytes2human(psutil.virtual_memory()[3]))
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string('RAM Used/Free')
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string(ramUsed+'/'+ramFree)
+        time.sleep(Refresh)
+    else:
+        lcd.clear()
+
+    # Disk
+    for _ in range(Transition):
+        diskTotal = (bytes2human(psutil.disk_usage('/')[0]))
+        diskFree = (bytes2human(psutil.disk_usage('/')[2]))
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string('SD Total/Free')
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string(diskTotal+'/'+diskFree)
+        time.sleep(Refresh)
+    else:
+        lcd.clear()
+
+    for _ in range(Transition):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+        IPAddress = s.getsockname()[0]
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string('IP Address')
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string(IPAddress)
+        time.sleep(Refresh)
+    else:
+        lcd.clear()
+
     
-    # cpu used
-    lcd.clear()
-    lcd.cursor_pos = (0, 0)
-    lcd.write_string('CPU Usage: ')
-    lcd.cursor_pos = (1, 0)
-    lcd.write_string(cpu)
-    time.sleep(3)
-    # cpu temp
-    lcd.clear()
-    lcd.cursor_pos = (0, 0)
-    lcd.write_string('CPU Temperature: ')
-    lcd.cursor_pos = (1, 0)
-    lcd.write_string(temp)
-    time.sleep(3)
-    # ram free 
-    lcd.clear()
-    lcd.cursor_pos = (0, 0)
-    lcd.write_string('Memory Free')
-    lcd.cursor_pos = (1, 0)
-    lcd.write_string(ramFree)
-    time.sleep(3)
-    # ram used
-    lcd.clear()
-    lcd.cursor_pos = (0, 0)
-    lcd.write_string('Memory Used')
-    lcd.cursor_pos = (1, 0)
-    lcd.write_string(ramUsed)
-    time.sleep(3)
 
 
     
